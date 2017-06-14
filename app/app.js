ed098d4ec453399dbe82294fd2502dc7ade91f42
Shaunak Das <shaunakdas2020@gmail.com>
2017-06-14 22:34:20 +0530

    Basic Design and API integration finished.

--- app/app.js
+++ app/app.js
@@ -2,13 +2,25 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
+   'ngAnimate',
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+   'ngAria',
+   'ngMaterial',
+  'chart.js',
+   'ngMessages'
 ]).run(['$rootScope','$http',function($rootScope, $http) {
   $rootScope.base_url_api = "http://localhost:3000/"
-}]).
+}]).service('userService', function(){
+	this.setResponse = function(data) {
+	        this.responseData = data;
+	};
+	this.getResponse = function() {
+	        return this.responseData;
+	};
+}).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,14 +9,54 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope','$http', function ($scope, $http) {
-    $http.get('https://66221cf0.ngrok.io/api/reference_scores.json')
+.controller('View1Ctrl', ['$scope','$http','userService', function ($scope, $http, userService) {
+    $http.get('http://localhost:3000/api/analytics/get_analytics?rollno=16437970&exam_id=1')
         .success(function(data, status, headers, config) {
-          $scope.details = data;
+          // $scope.details = data
+          userService.setResponse(data);
+          $scope.details = userService.getResponse();
         })
         .error(function(data, status, headers, config) {
           // log error
     });
+
+        $scope.diffAnalysisInfoTitle = "More about Difficulty Analysis";
+        $scope.diffAnalysisInfoContent = "In this section, you can see analysis of your attempt based on different difficulty levels. With respect to each level of difficulty, you can see total questions in the paper and number of questions correctly attempted by you.";
+        $scope.showDialogInfo = function(ev,title,content) {
+            $mdDialog.show(
+              $mdDialog.alert()
+                .clickOutsideToClose(true)
+                .title(title)
+                .textContent(content)
+                .ariaLabel('Alert Dialog Demo')
+                .ok('Got it!')
+                .targetEvent(ev)
+            );
+
+          };
+          // Difficulty Breakup Graph section
+        $scope.colors = ['#f44242', '#42f4f1', '#40E0D0'];
+        $scope.labels = ['Total', 'Easy', 'Medium', 'Tough'];
+        $scope.series = ['Total', 'Correct'];
+        $scope.data = [
+          [20, 10, 5, 5],
+          [10, 4, 2, 3]
+        ]; 
+        var starts_zero = {
+            ticks: {
+                beginAtZero:true
+            }
+        };
+        $scope.difficulty_options = {
+          scales: {
+              xAxes: [starts_zero],
+              yAxes: [starts_zero]
+          }
+        };
+        $scope.datasetOverride = {
+          label: '# of Votes'
+        }
+
 }]);
 
 
